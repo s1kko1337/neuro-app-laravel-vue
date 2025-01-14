a97181eb7998025553af1f51e9b18279892a7984
Mike <Mike>
2025-01-14 15:29:17 +0300

    initial commit

---
+++ resources/js/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue';
+
+const username = ref("Vue test name");
+</script>
+
+<template>
+  <h1>Hello, {{ username }}!</h1>
+</template>
+
+<style scoped>
+
+</style>
---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    // server: {
+    //     strictPort : true , 
+    //     host : true, 
+    //     port: 5173,
+    //     origin : "http://127.0.0.1:5173" , 
+    //   },
+    plugins: [
+        vue(),
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+    build: {
+        outDir: 'public/build',
+        manifest: true,
+    },
+});
